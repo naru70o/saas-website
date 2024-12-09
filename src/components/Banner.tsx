b02ea499c251646347d0be9c2a19e7891478902c
kadar naruto <khadarpaashe123@gmail.com>
2024-12-09 10:52:33 +0300

    builded the banner and nav sections

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,17 @@
+import "./banner.css";
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center banner">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline-block">
+            Introducing a completely redesigned interface —
+          </span>
+          <a href="#" className="underline underline-offset-4 font-medium">
+            Explore the Demo
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,65 @@
+import Image from "next/image";
+import navLogo from "../assets/images/logosaas.png";
+import MenuIcon from "../assets/icons/menu.svg";
 export const Navbar = () => {
-  return null;
+  return (
+    <div className="bg-black">
+      <div className="px-4">
+        <div className="py-4 flex items-center justify-between">
+          <div className="relative">
+            <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] blur-md"></div>
+            <Image
+              src={navLogo}
+              alt="Saas logo"
+              className="h-12 w-12 relative"
+            />
+          </div>
+          {/* icon */}
+          <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden">
+            <MenuIcon className="text-white" />
+          </div>
+
+          {/* Message */}
+          <nav className="hidden sm:flex items-center text-white gap-6">
+            <a
+              href="#"
+              className="opacity-60 text-white hover:text-opacity-100"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="opacity-60 text-white hover:text-opacity-100"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="opacity-60 text-white hover:text-opacity-100"
+            >
+              Features
+            </a>
+            <a
+              href="#"
+              className="opacity-60 text-white hover:text-opacity-100"
+            >
+              Updates
+            </a>
+            <a
+              href="#"
+              className="opacity-60 text-white hover:text-opacity-100"
+            >
+              Customers
+            </a>
+            <a
+              href="#"
+              className="text-black bg-white rounded-md px-4 py-2 hover:text-opacity-100"
+            >
+              Get for free
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
