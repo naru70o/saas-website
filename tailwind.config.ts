05ed12b6320232117607fbea92eacc726afd5afa
kadar naruto <khadarpaashe123@gmail.com>
2024-12-19 10:37:12 +0300

    fixed an another bug and used typescript rules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,15 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/_components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       container: {
-        padding: '1rem', 
+        padding: "1rem",
         center: true,
-      }
+      },
     },
   },
   plugins: [],
